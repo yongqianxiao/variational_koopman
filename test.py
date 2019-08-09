e68c014eacaaeef4c2068b0e4ad0c41d6092a4d8
yongqianxiao <shawyongqian@gmail.com>
2019-08-09 09:23:43 +0800

    测试.gitignore文件是否可行

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-import parser
+import argparse
 
+parser = argparse.ArgumentParser()
 parser.add_argument('--extractor_size', nargs='+', type=int, default=[32],
                     help='hidden layer sizes in feature extractor/decoder')
 args = parser.parse_args()
-print('extrator_size', args.extrator_size)
+print('extrator_size', args.extractor_size)
